672cd31a8fe2412ddee58a792a002c181d93d2ab
Dalvik <qq915458022@users.noreply.github.com>
2016-09-11 14:35:41 +0800

    Add files via upload

---
+++ Plugin.php
@@ -0,0 +1,104 @@
+﻿<?php
+/**
+ * 署名我的知识产权 < Creative Common
+ *
+ * @category tools
+ * @package Creative Common
+ * @author qq915458022
+ * @version 1.0
+ * @link https://ifengge.cn
+ */
+class CC_Plugin implements Typecho_Plugin_Interface
+{
+		private static $_defaultConfig = array(
+        'default_license' => 'by-nc-sa',
+    );
+    
+    /**
+     * 激活插件方法,如果激活失败,直接抛出异常
+     *
+     * @access public
+     * @return void
+     * @throws Typecho_Plugin_Exception
+     */
+    public static function activate()
+    {
+        Typecho_Plugin::factory('Widget_Archive')->singleHandle = array('CC_Plugin', 'singleHandle');
+		}
+    
+    /**
+     * 获取插件配置面板
+     *
+     * @access public
+     * @param Typecho_Widget_Helper_Form $form 配置面板
+     * @return void
+     */
+    public static function config(Typecho_Widget_Helper_Form $form)
+    {
+    		$defaultConfig = self::getDefaultConfig(); //获取配置
+    		
+    		//设置默认协议
+        $name = new Typecho_Widget_Helper_Form_Element_Select('default_license',array('no' => '不保留权利' , 'by' => '署名（BY）','by-nd' => '署名（BY）- 禁止演绎（ND）','by-nc' => '署名（BY）- 非商业性使用（NC）','by-sa' => '署名（BY）- 相同方式共享（SA）','by-nc-nd' => '署名（BY）- 非商业性使用（NC）- 禁止演绎（ND）','by-nc-sa' => '署名（BY）- 非商业性使用（NC）- 相同方式共享（SA）' , 'all' => '保留所有权利'),$defaultConfig->default_license,_t('默认证书'),_t('所有没有单独注明的文章均会自动采用该协议进行授权'));
+        $form->addInput($name);
+    }
+    
+    /**
+     * 个人用户的配置面板
+     *
+     * @access public
+     * @param Typecho_Widget_Helper_Form $form
+     * @return void
+     */
+    public static function personalConfig(Typecho_Widget_Helper_Form $form){}
+
+    /**
+     * 禁用插件方法,如果禁用失败,直接抛出异常
+     *
+     * @static
+     * @access public
+     * @return void
+     * @throws Typecho_Plugin_Exception
+     */
+    public static function deactivate(){}
+        
+    /**
+     * 插件的实现方法
+     *
+     * @access public
+     * @return void
+     */
+    
+    public static function singleHandle($archive,$select) 
+    {
+        $currentPath = Helper::options()->pluginUrl . '/CC/';
+
+        echo '<script type="text/javascript" src="https://static.ifengge.cn/js/jquery.min.js"></script>' . "\n";
+        
+        $license = self::getConfig() -> default_license;
+        
+        //在此为每个页面进行单独设置
+        switch($archive->cid){
+        	case 121:
+        		$license = "all";
+        		break;
+        }
+        echo '<script type="text/javascript" src="' . $currentPath . 'scripts/ccommon.js?license='.$license.'"></script>' . "\n";
+    }
+    
+    private static function getDefaultConfig($key = null)
+    {
+        if( isset($key) )
+            return self::$_defaultConfig[$key];
+        return (object)self::$_defaultConfig;
+    }
+    
+    private static function getConfig($key = null){
+    	if(isset($key)){
+    		$options = Helper::options();
+        $plugin_options = $options -> plugin('CC');
+        return $plugin_options -> $key;
+      }
+      return (object)(Helper::options() -> plugin('CC'));
+    }
+}
+ 
---
+++ scripts/ccommon.js
@@ -0,0 +1,60 @@
+﻿var license = getQueryString(getJSUrl(),"license");
+var jsPath =  getJSUrl().substring(0, getJSUrl().lastIndexOf('/'));
+var path = jsPath.substring(0, jsPath.lastIndexOf('/')); 
+$( document ).ready(function() {
+	if(license == "all"){
+		var node=document.createTextNode("保留所有权利。");
+		makeLicense(node,null,null);
+	}else if(license == "no"){
+		var node=document.createTextNode("针对此文章不进行权利保留：");
+		makeLicense(node,"https://creativecommons.org/publicdomain/zero/1.0/",path + "/media/zero/1.0/80x15.png");
+	}else{
+		var node=document.createTextNode("本作品基于此协议授权：");
+		makeLicense(node,"https://creativecommons.org/licenses/" + license + "/4.0/",path + "/media/cc/" + license +"/4.0/80x15.png");
+	}
+})
+
+function makeLicense(node,llink,logo){
+	var para=document.createElement("h6");
+	para.style.cssText = "margin-top:50px";
+	
+	para.appendChild(node);
+	
+	if(llink != null){
+		var link,img;
+		para.appendChild(link = document.createElement("a"));
+		link.appendChild(img = document.createElement("img"));
+		link.target = "_blank";
+		link.href = llink;
+		img.style.cssText = "vertical-align:text-top";
+		img.src = logo;
+	}
+		
+	var element=document.getElementById("article-content-div");
+	element.appendChild(para);
+}
+
+function getQueryString(url, name) {
+    var reg = new RegExp("(^|&|\\?)" + name + "=([^&]*)(&|$)", "i");
+    var r = url.substr(1).match(reg);
+    if (r != null) return unescape(r[2]); return null;
+} 
+
+function getJSUrl(){
+	var i = 0,
+    	result = null,
+        script, scripts, url, reg, r;
+    
+    // firefox支持currentScript属性
+    if( document.currentScript ){
+        script = document.currentScript
+    }
+    else{
+        // 正常情况下，在页面加载时，当前js文件的script标签始终是最后一个
+        scripts = document.getElementsByTagName( 'script' )            
+        script = scripts[ scripts.length - 1 ]
+    }           
+
+    url = script.hasAttribute ? script.src : script.getAttribute( 'src', 4 );
+    return url;
+}
